35082993dc6f889b12c3be584fd204f286d43b94
VorteX45 <vortex456@rambler.ru>
2017-05-14 18:37:34 +0300

    Version change

--- src/qt/gvidonstrings.cpp
+++ src/qt/gvidonstrings.cpp
@@ -313,7 +313,7 @@ QT_TRANSLATE_NOOP("gvidon-core", "Connect to KeePassHttp on port <port> (default
 QT_TRANSLATE_NOOP("gvidon-core", "Connect to a node to retrieve peer addresses, and disconnect"),
 QT_TRANSLATE_NOOP("gvidon-core", "Connection options:"),
 QT_TRANSLATE_NOOP("gvidon-core", "Copyright (C) 2009-%i The Bitcoin Core Developers"),
-QT_TRANSLATE_NOOP("gvidon-core", "Copyright (C) 2014-%i The Gvidon Core Developers"),
+QT_TRANSLATE_NOOP("gvidon-core", "Copyright (C) 2017-%i The Gvidon Core Developers"),
 QT_TRANSLATE_NOOP("gvidon-core", "Corrupted block database detected"),
 QT_TRANSLATE_NOOP("gvidon-core", "Could not parse masternode.conf"),
 QT_TRANSLATE_NOOP("gvidon-core", "Debugging/Testing options:"),
